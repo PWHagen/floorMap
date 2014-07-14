5810f25ec5dbfec9fd2409c694e14687d1307446
PWHagen <patrickwhagen@gmail.com>
2014-07-13 21:15:06 -0500

    Committing initial code base and README. Currently I have the page loading and most of the navigation complete. Need to work on search functionality and backend services to fill the database.

---
+++ BuildingClass.py
@@ -0,0 +1,30 @@
+__author__ = 'Patrick'
+
+
+class Building:
+    def __init__(self, id, num_floors):
+        self.isActive = False
+        self.curr_floor = 1
+        self.building_id = str(id)
+        self.num_floors = num_floors
+
+    def is_active(self):
+       return self.isActive
+
+    def activate(self):
+        self.isActive = True
+
+    def deactivate(self):
+        self.isActive = False
+
+    def update_floor(self, floor):
+        self.curr_floor = int(floor)
+
+    def current_floor(self):
+        return self.curr_floor
+
+    def building_id(self):
+        return self.building_id
+
+    def num_floors(self):
+        return self.num_floors
---
+++ floorMap.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect, url_for
+from BuildingClass import *
+
+app = Flask(__name__)
+
+# ------------Gloabal Vars--------------#
+floors = ["Floor 1", "Floor 2", "Floor 3", "Floor 4", "Floor 5", "Floor 6", "Floor 7", "Floor 8"]
+building_a = Building('a', 2)
+building_b = Building('b', 5)
+building_c = Building('c', 8)
+#Need to move to more object oriented approach
+#--------------------------------------#
+
+#----------Page Response Functions-----#
+@app.route('/')
+def main():
+    return render_template('index.html', floor_image='../static/img/nic.jpg', active='z')
+
+@app.route('/mopac_a')
+def mopac_a():
+    building_a.activate()
+    building_b.deactivate()
+    building_c.deactivate()
+    return reload()
+
+@app.route('/mopac_b')
+def mopac_b():
+    building_a.deactivate()
+    building_b.activate()
+    building_c.deactivate()
+    return reload()
+
+@app.route('/mopac_c')
+def mopac_c():
+    building_a.deactivate()
+    building_b.deactivate()
+    building_c.activate()
+    return reload()
+
+@app.route("/floor/<int:floor_num>")
+def floor(floor_num):
+    if building_a.isActive:
+        building_a.update_floor(floor_num)
+    elif building_b.isActive:
+        building_b.update_floor(floor_num)
+    else:
+        building_c.update_floor(floor_num)
+    return reload()
+
+
+#----------Page Response Functions----#
+
+#----------Helper Function------------#
+def reload():
+    if building_a.isActive:
+        return render_template('index.html',
+                               floor_image='../static/img/nic' +building_a.building_id +str(building_a.current_floor())+ '.jpg'
+                               , active_a='active', floors=floors[0:building_a.num_floors])
+    elif building_b.isActive:
+        return render_template('index.html',
+                               floor_image='../static/img/nic' + building_b.building_id + str(building_b.current_floor()) + '.jpg',
+                               active_b='active', floors=floors[0:building_b.num_floors])
+    else:
+        return render_template('index.html',
+                               floor_image='../static/img/nic' + building_c.building_id + str(building_c.current_floor()) + '.jpg'
+                               , active_c='active', floors=floors[0:building_c.num_floors])
+
+
+if __name__ == '__main__':
+    app.run(host='10.0.1.9')
