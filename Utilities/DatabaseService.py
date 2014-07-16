fe8178c7f9e58367fd9591fcbde58eaf42330b1a
Patrick Hagen <patrickwhagen@gmail.com>
2014-07-16 16:20:23 -0500

    Adding DatabaseService.py in the hopes that I can get the code to run as a service on my machine. Was able to get the service registered but currently it wont run. May resort to bundling the script into an EXE and scheduling a task to run it periodically.

---
+++ Utilities/DatabaseService.py
@@ -0,0 +1,50 @@
+__author__ = 'phagen'
+
+
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import win32api
+import win32evtlogutil
+
+from Utilities import ParsePhoneBook
+
+
+
+class FloorMapSvc (win32serviceutil.ServiceFramework):
+
+    _svc_name_ = "FloorMapSvc"
+    _svc_display_name_ = "Floor Map Service"
+
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.setevent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        self.timeout = 120000
+
+        while 1:
+            #Wait for service stop signal, if timeout, loop again
+            rc = win32event.waitforsingleobject(self.hWaitStop, self.timeout)
+            #Check to see if self.hWaitStop Happened
+            if rc == win32event.WAIT_OBJECT_0:
+                servicemanager.LogInfoMsg("FloorMapSvc Stop")
+                break
+            else:
+                ParsePhoneBook.parse()
+
+def ctrlHandler(ctrlType):
+    return True
+
+if __name__ == "__main__":
+    win32api.SetConsoleCtrlHandler(ctrlHandler,True)
+    win32serviceutil.HandleCommandLine(FloorMapSvc)
+
+
