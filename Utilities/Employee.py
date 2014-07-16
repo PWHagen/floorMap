352828e1274c38dd4c825dd74de56d2d0d421941
Patrick Hagen <patrickwhagen@gmail.com>
2014-07-16 14:05:35 -0500

    JSON parsing works but needs to add in the database connection. I think it will be best to create a database class to handle that interaction and abstract it from this code. It will also be helpful to do this when I just the database class in my model.
    Updated Employee.py to include the getters and setters that are necessary for each person. I think there is a way to use decorators or setattr to make this cleaner

--- Utilities/Employee.py
+++ Utilities/Employee.py
@@ -1,15 +1,36 @@
 __author__ = 'Patrick Hagen'
 
+
 class Employee:
     def __init__(self):
         self.name = ""
         self.cube = ""
         self.department = ""
+        self.phone_number = ''
 
     def set_name(self, name):
         self.name = name
 
     def get_name(self):
         return self.name
+
+    def set_cube(self, cube):
+        self.cube = cube
+
+    def get_cube(self):
+        return self.cube
+
+    def set_department(self, department):
+        self.department = department
+
+    def get_department(self):
+        return self.department
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    def get_phone_number(self):
+        return self.phone_number
+
 
 
--- Utilities/ParsePhoneBook.py
+++ Utilities/ParsePhoneBook.py
@@ -5,33 +5,37 @@ import re
 from Utilities.Employee import *
 
 
-url = 'http://niweb-notes.natinst.com/operations/niphone.nsf/FirstNameWeb?readviewentries&outputformat=JSON&OpenView&Start=20.480&Count=1000&Expand=20#20'
-f = urllib.request.urlopen(url)
-charset = f.info().get_param('charset', 'utf8')
-print(charset)
-data = f.read()
-data = data.decode(charset)
-data = re.sub('(?!\\\\r)[\\\\].', '', data)
-decoded = json.loads(data)
-print(json.dumps(data, indent=4))
-viewEntries = decoded['viewentry']
-view = viewEntries[20]['entrydata']
-info = view[0]['text']
-print(info['0'])
-print()
 employee_dict = dict()
-for i in range(0, 26):
-    print (i)
-    url = 'http://niweb-notes.natinst.com/operations/niphone.nsf/' \
-          'FirstNameWeb?readviewentries&outputformat=JSON&OpenView&Start=1&Count=1000&Expand=' + str(i+1) + '#20'
-    f = urllib.request.urlopen(url)
-    charset = f.info().get_param('charset', 'utf8')
-    data = f.read()
-    data = data.decode(charset)
-    data = re.sub('(?!\\\\r)[\\\\].', '', data)
-    decoded = json.loads(data)
-    viewEntries = decoded['viewentry']
-    view = viewEntries[i+1]['entrydata']
-    info = view[0]['text']
-    print(info.keys())
 
+for i in range(0, 26):  # Iterate over URLs for each letter (26 in the alphabet)
+    #-----Use Count = 1000 so that the page does not get clipped which causes missing data in JSON--------#
+    url = 'http://niweb-notes.natinst.com/operations/niphone.nsf/' \
+          'FirstNameWeb?readviewentries&outputformat=JSON&OpenView&Start=1&Count=1000&Expand=' + str(i+1) + '#' + str(i+1)
+    #-----Use Count = 1000 so that the page does not get clipped which causes missing data in JSON--------#
+
+    page = urllib.request.urlopen(url)
+    charset = page.info().get_param('charset', 'utf8')  # Determine the character set of the JSON
+    data = page.read()
+    data = data.decode(charset) # Decode the bytes to JSON string
+    data = re.sub('(?!\\\\r)[\\\\].', '', data)  # JSON has bad escape characters fixed with this substitution
+    decoded = json.loads(data)
+    viewEntries = decoded['viewentry']  # All data broken down by letter
+    view = viewEntries[i+1]['entrydata']  # Gives first person in a letter group
+    x = i + 1
+    while str(i+1) + '.' in viewEntries[x]['@position']:
+        view = viewEntries[x]['entrydata']
+        employee = Employee()
+        employee.set_name(view[0]['text']['0'])  # Name JSON Field
+        employee.set_phone_number(view[1]['text']['0'])  # Phone Number JSON Field
+        employee.set_department(view[5]['text']['0'])  # Department JSON Field
+        employee.set_cube(view[7]['text']['0'])  # Cube JSON Field
+        employee_dict[employee.get_name()] = employee  # Eventually I will make this an entry to the DB
+
+        x += 1  # Increment through array of names
+
+name = input('Enter a name you would like to look up \n')
+person = employee_dict[name]
+print(person.get_name())
+print(person.get_phone_number())
+print(person.get_department())
+print(person.get_cube())
